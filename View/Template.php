fd925fd42567dafda3ebcc6c03204b83f629ca7e
Katherine <katherineprintz@gmail.com>
2021-03-01 16:29:00 -0700

    Added logout functionality
    Added logout button to database edit page which sends user back to login
    page and forces new login. Also edited Template class to set the
    controller variable to the controller (from the URI) and edited view.json to display
    pages based on just the controller as well as added different footers
    for different controllers.

--- View/Template.php
+++ View/Template.php
@@ -13,11 +13,11 @@ class Template
     public function __construct()
     {
         //todo probably don't want to set hooter and feader this way since they may not always be the same
-        $this->viewFile = file_get_contents("View/Calculations/Index/view.json");
+        $this->viewFile = file_get_contents("View/view.json");
         $this->data = json_decode($this->viewFile, true);
+        $this->controller = [explode('/', $_SERVER['REQUEST_URI'])][0][1];
         $this->header = $this->data['Header']['body'];
-        $this->footer = $this->data['Footer']['body'];
-        $this->controller = $_SERVER['REQUEST_URI'];
+        $this->footer = $this->data['Footer'][$this->controller];
     }
 
     public function setData($key, $data)
@@ -28,8 +28,6 @@ class Template
 
     public function render()
     {
-        //todo parse relevant JSON file - and render its elements (for this controller path)
-
         extract([$this]);
         ob_start();
         include $this->header;
